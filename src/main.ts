d089deda6bb971feb763ab79d1f247e2635d14e1
sankeyangshu <1260323835@qq.com>
2023-03-26 20:45:07 +0800

    build: 多环境配置

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  * @Author: 三棵杨树
  * @Date: 2023-03-26 16:04:40
  * @LastEditors: 三棵杨树
- * @LastEditTime: 2023-03-26 16:46:00
+ * @LastEditTime: 2023-03-26 18:53:46
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -12,4 +12,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
