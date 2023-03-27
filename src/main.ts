b975fd31d946b1620005ed58a04cf368d1fa212f
sankeyangshu <1260323835@qq.com>
2023-03-27 21:22:51 +0800

    feat: 新增启动服务配置

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,20 @@
  * @Author: 三棵杨树
  * @Date: 2023-03-26 16:04:40
  * @LastEditors: 三棵杨树
- * @LastEditTime: 2023-03-26 18:53:46
+ * @LastEditTime: 2023-03-27 21:21:48
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Config from 'config';
+
+// 获取本地服务配置
+const config = Config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || config.port;
+  await app.listen(port);
 }
 
 bootstrap();
