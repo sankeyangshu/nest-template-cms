fb224306beff4b12df350bf33ac2765a669afb0f
sankeyangshu <1260323835@qq.com>
2023-05-04 17:12:12 +0800

    feat: 修改post请求返回的状态码

--- src/interceptors/response.interceptor.ts
+++ src/interceptors/response.interceptor.ts
@@ -1,5 +1,11 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Observable, map } from 'rxjs';
 
 /**
@@ -16,12 +22,16 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, ResBody<T>> {
     context: ExecutionContext,
     next: CallHandler<T>
   ): Observable<ResBody<T>> | Promise<Observable<ResBody<T>>> {
-    // const ctx = context.switchToHttp();
-    // const req = ctx.getRequest<Request>();
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest<Request>();
+    const res = ctx.getResponse<Response>();
     // console.log('这里在全局响应拦截器执行之前', req);
     return next.handle().pipe(
       map((data: T) => {
         // console.log('这里在拦截器执行之后', data);
+        if (req.method === 'POST') {
+          if (res.statusCode === 201) res.status(HttpStatus.OK);
+        }
         return {
           code: 0,
           data: data,
